22e7b2edfe0a2da6155aa4620339becf1c6e8788
Marcos8060 <marcosgav80@gmail.com>
2022-03-13 03:45:19 +0300

    created the project model

--- prohub/models.py
+++ prohub/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    image = models.ImageField(upload_to='projects')
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    pub_date = models.DateTimeField()
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
